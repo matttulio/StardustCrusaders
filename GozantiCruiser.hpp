d9e0e123c2001b50bcf7414106339036db7b985f
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:37 +0200

    Create GozantiCruiser.hpp

---
+++ GozantiCruiser.hpp
@@ -0,0 +1,49 @@
+//
+//  GozantiCruiser.hpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#ifndef GozantiCruiser_hpp
+#define GozantiCruiser_hpp
+
+#include <stdio.h>
+#include "ship.hpp"
+
+class gozanticruiser: public ship{
+    
+public:
+    
+    gozanticruiser();
+    //superstardestroyer(int dim, string name, char code, int x, int y, bool d, bool o);
+
+    int getSize() const;
+    int getX() const;
+    int getY() const;
+    string getName() const;
+    char getCode() const;
+
+
+    bool ShipOrientation() const;
+    bool ShipDestroyed() const;
+    
+    
+    void setPosition(int x, int y, bool o);
+    void printShip();
+    bool Hit(int xHit, int yHit);
+
+private:
+    int shipSize;
+
+    int theX, theY;
+
+    bool destroyed;
+    bool orientation;
+
+    string shipName;
+    char shipCode;
+    
+};
+
+#endif /* GozantiCruiser_hpp */
