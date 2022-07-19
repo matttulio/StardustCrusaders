a8485a3b450e06d05f80ccfd0af2004017140219
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:31 +0200

    Create GozantiCruiser.cpp

---
+++ GozantiCruiser.cpp
@@ -0,0 +1,18 @@
+//
+//  GozantiCruiser.cpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#include "GozantiCruiser.hpp"
+
+gozanticruiser::gozanticruiser(){
+    
+    shipSize = 3;
+    
+    shipName = "Gozanti Cruiser";
+    
+    shipCode = 'C';
+    
+}
