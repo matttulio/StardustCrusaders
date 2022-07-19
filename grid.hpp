0e238daf4d8075a1f61ce252cb0c70e297da6a94
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:41 +0200

    Update grid.hpp

--- grid.hpp
+++ grid.hpp
@@ -10,27 +10,35 @@
 
 #include <string>
 #include <vector>
-#include "constants.hPP"
+#include "constants.hpp"
+#include "Game.hpp"
 #include "ship.hpp"
+
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
 
 class grid{
 
 public:
     grid();
-    grid(const grid &oldGrid);
-    grid& operator=(const grid &right);
-    ~grid();
+    grid(game m);
 
+    void printInfo();
     void printPlayerGrid();
+    void setGrid(bool player_mode);
     void printOpponentGrid();
-    bool setShip(int shipNum, int x, int y, bool o);
     bool shot(int x, int y);
 
 
 
 private:
-   char theGrid[grid_dim][grid_dim];
-   std::vector<ship> shipVec;
+    int dim_grid;
+    vector< vector<char> > theGrid; //usiamo la struttura vector così da potere decidere in seguito la dimensione della nostra matrice, se avessi usato un semplice array avrei dovuto definirla a priori dal codice e non avrei potuto deciderla una volta avviato il programma
+    vector<char> row;   //riempiamo prima le righe e poi usiamo push_back per metterle dentro la matrice, dopo di che si gestisce come un semplice array
+    int n_ships;
+    vector<ship> shipVec;
 
 
 };
