3bbedbd3c45123d907c3bb12b9f56e39c9bbc7bb
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:56 +0200

    Create SuperStarDestroyer.hpp

---
+++ SuperStarDestroyer.hpp
@@ -0,0 +1,49 @@
+//
+//  SuperStarDestroyer.hpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#ifndef SuperStarDestroyer_hpp
+#define SuperStarDestroyer_hpp
+
+#include <stdio.h>
+#include "ship.hpp"
+
+class superstardestroyer: public ship{
+    
+public:
+    
+    superstardestroyer();  
+    //superstardestroyer(int dim, string name, char code, int x, int y, bool d, bool o);
+
+    int getSize() const;
+    int getX() const;
+    int getY() const;
+    string getName() const;
+    char getCode() const;
+
+
+    bool ShipOrientation() const;
+    bool ShipDestroyed() const;
+    
+    
+    void setPosition(int x, int y, bool o);
+    void printShip();
+    bool Hit(int xHit, int yHit);
+
+private:
+    int shipSize;
+
+    int theX, theY;
+
+    bool destroyed;
+    bool orientation;
+
+    string shipName;
+    char shipCode;
+    
+};
+
+#endif /* SuperStarDestroyer_hpp */
