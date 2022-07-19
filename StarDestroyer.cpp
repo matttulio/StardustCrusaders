9b3034d77b862ed434e0ea183b20e09770abf159
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:50 +0200

    Create StarDestroyer.cpp

---
+++ StarDestroyer.cpp
@@ -0,0 +1,18 @@
+//
+//  StarDestroyer.cpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#include "StarDestroyer.hpp"
+
+stardestroyer::stardestroyer(){
+    
+    shipSize = 4;
+    
+    shipName = "Star Destroyer";
+    
+    shipCode = 'B';
+    
+}
