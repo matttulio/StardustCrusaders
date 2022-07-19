b51d320b990d699d1344671ad9f7327ab708dd34
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:23 +0200

    Update constants.hpp

--- constants.hpp
+++ constants.hpp
@@ -10,18 +10,17 @@
 #ifndef CONSTANTS_HPP
 #define CONSTANTS_HPP
 
-const int n_ship = 4;
 const int ship_dim[n_ship] = {5, 4, 3, 1};
 const int ship_spaces = 13;
-const std::string ship_name[n_ship] = {"Super Star Destroyer", " Star Destroyer", " Gozanti Cruiser", "TIE Fighter"};
+const std::string ship_name[n_ship] = {"Super Star Destroyer", " Star Destroyer", " Gozanti Cruiser", "TIE Fighter"};   //forse fare due array distinti con nomi diversi per le due forza in gioco
 const char ship_code[n_ship] = {'A', 'B', 'C', 'D'};
 
-const char water = '~';
-const char hit = 'X';
-const char miss = 'O';
+const char unknown = '?';
+const char hit = '/';
+const char sunk = 'X';
+const char miss = '~';
 
 
-const int grid_dim = 10;
 
 
 #endif /* constants_hpp */
