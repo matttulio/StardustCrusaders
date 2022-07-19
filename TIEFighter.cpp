d220ac976e57a1f3f6c6a7f6a22ba3c842b91ad3
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:58:01 +0200

    Create TIEFighter.cpp

---
+++ TIEFighter.cpp
@@ -0,0 +1,18 @@
+//
+//  TIEFighter.cpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#include "TIEFighter.hpp"
+
+TIEfighter::TIEfighter(){
+    
+    shipSize = 1;
+    
+    shipName = "TIEfighter";
+    
+    shipCode = 'D';
+    
+}
