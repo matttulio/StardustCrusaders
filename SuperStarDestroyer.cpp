865963e8ac26eec702f7e988cb31114b84ee7507
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-19 10:57:53 +0200

    Create SuperStarDestroyer.cpp

---
+++ SuperStarDestroyer.cpp
@@ -0,0 +1,18 @@
+//
+//  SuperStarDestroyer.cpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#include "SuperStarDestroyer.hpp"
+
+superstardestroyer::superstardestroyer(){
+    
+    shipSize = 5;
+    
+    shipName = "Super Star Destroyer";
+    
+    shipCode = 'A';
+    
+}
