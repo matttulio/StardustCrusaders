ea429187aca4b6c586b9eb46f45a37cbbc7f5728
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-25 10:19:21 +0200

    Update grid.hpp

--- grid.hpp
+++ grid.hpp
@@ -5,8 +5,8 @@
 //  Created by Matteo Gallo on 09/04/22.
 //
 
-#ifndef GRID_H_INCLUDED
-#define GRID_H_INCLUDED
+#ifndef GRID_HPP_INCLUDED
+#define GRID_HPP_INCLUDED
 
 #include <string>
 #include <vector>
@@ -23,22 +23,28 @@ class grid{
 
 public:
     grid();
-    grid(game m);
+    grid(game m, bool player_mode);
 
     void printInfo();
     void printPlayerGrid();
     void setGrid(bool player_mode);
     void printOpponentGrid();
-    bool shot(int x, int y);
+    bool isShotBy(grid board);//grid indica il tipo e board il nome
 
 
 
 private:
     int dim_grid;
+    bool p_mode;
     vector< vector<char> > theGrid; //usiamo la struttura vector così da potere decidere in seguito la dimensione della nostra matrice, se avessi usato un semplice array avrei dovuto definirla a priori dal codice e non avrei potuto deciderla una volta avviato il programma
     vector<char> row;   //riempiamo prima le righe e poi usiamo push_back per metterle dentro la matrice, dopo di che si gestisce come un semplice array
     int n_ships;
     vector<ship> shipVec;
+    
+    int *r = new int(0);
+    int *orientation = new int(0);
+    bool *already_hit = new bool(false);
+    int *Try_z = new int(0);
 
 
 };
