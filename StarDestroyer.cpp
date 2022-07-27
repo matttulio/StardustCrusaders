92598e3bc13fde778eb30a90636ac44523c22aed
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-27 12:34:31 +0200

    Update StarDestroyer.cpp

--- StarDestroyer.cpp
+++ StarDestroyer.cpp
@@ -8,15 +8,98 @@
 #include "StarDestroyer.hpp"
 
 stardestroyer::stardestroyer(){
-    
-}
-
-stardestroyer::stardestroyer(int dim, string name, char code):ship(dim, name, code){
-    
     shipSize = 4;
     
     shipName = "Star Destroyer";
     
     shipCode = 'B';
     
+    health = shipSize;
+}
+
+stardestroyer::stardestroyer(int dim, string name, char code){
+    
+    shipSize = dim;
+    
+    shipName = name;
+    
+    shipCode = code;
+    
+    health = shipSize;
+    
+}
+
+int stardestroyer::getSize() const{
+    return shipSize;
+}
+
+
+int stardestroyer::getX() const{
+    return theX;
+}
+
+
+int stardestroyer::getY() const{
+    return theY;
+}
+
+
+string stardestroyer::getName() const{
+    return shipName;
+}
+
+
+char stardestroyer::getCode() const{
+    return shipCode;
+}
+
+
+bool stardestroyer::ShipOrientation() const{
+    return orientation;
+}
+
+
+bool stardestroyer::ShipDestroyed() const{
+    return destroyed;
+}
+
+
+void stardestroyer::setShip(int x, int y, bool o){
+    
+    theX = x;
+    theY = y;
+    
+    orientation = o;
+    
+    return;
+}
+
+
+void stardestroyer::printShip(){
+    
+    for(int i = 0; i < shipSize; i++){
+        
+        cout << shipCode << " ";
+    }
+    
+    cout << endl;
+    
+    return;
+}
+
+
+void stardestroyer::setHit(){
+    
+    health--;
+    
+}
+
+bool stardestroyer::isSunk(){
+    
+    if(health == 0){ //se non ha salute è affondata
+        destroyed = true;
+        return true;
+    }else
+        return false;
+        
 }
