ee1ec79c0d8b92ac72b5e0e541ff726b1bbac8e5
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-27 12:34:40 +0200

    Update TIEFighter.cpp

--- TIEFighter.cpp
+++ TIEFighter.cpp
@@ -11,7 +11,7 @@ TIEfighter::TIEfighter(){
     
 }
 
-TIEfighter::TIEfighter(int dim, string name, char code):ship(dim, name, code){
+TIEfighter::TIEfighter(int dim, string name, char code){
     
     shipSize = 1;
     
@@ -19,4 +19,81 @@ TIEfighter::TIEfighter(int dim, string name, char code):ship(dim, name, code){
     
     shipCode = 'D';
     
+    health = shipSize;
+    
+}
+
+int TIEfighter::getSize() const{
+    return shipSize;
+}
+
+
+int TIEfighter::getX() const{
+    return theX;
+}
+
+
+int TIEfighter::getY() const{
+    return theY;
+}
+
+
+string TIEfighter::getName() const{
+    return shipName;
+}
+
+
+char TIEfighter::getCode() const{
+    return shipCode;
+}
+
+
+bool TIEfighter::ShipOrientation() const{
+    return orientation;
+}
+
+
+bool TIEfighter::ShipDestroyed() const{
+    return destroyed;
+}
+
+
+void TIEfighter::setShip(int x, int y, bool o){
+    
+    theX = x;
+    theY = y;
+    
+    orientation = o;
+    
+    return;
+}
+
+
+void TIEfighter::printShip(){
+    
+    for(int i = 0; i < shipSize; i++){
+        
+        cout << shipCode << " ";
+    }
+    
+    cout << endl;
+    
+    return;
+}
+
+
+void TIEfighter::setHit(){
+    
+    health--;
+    
+}
+
+bool TIEfighter::isSunk(){
+    
+    if(health == 0){ //se non ha salute è affondata
+        destroyed = true;
+        return true;
+    }else
+        return false;
+        
 }
