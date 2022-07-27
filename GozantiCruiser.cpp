513e96e5e707295a7a224c4a5a01ea68d98c10e1
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-27 12:34:19 +0200

    Update GozantiCruiser.cpp

--- GozantiCruiser.cpp
+++ GozantiCruiser.cpp
@@ -9,14 +9,99 @@
 
 gozanticruiser::gozanticruiser(){
     
-}
-
-gozanticruiser::gozanticruiser(int dim, string name, char code):ship(dim, name, code){
-    
     shipSize = 3;
     
     shipName = "Gozanti Cruiser";
     
     shipCode = 'C';
     
+    health = shipSize;
+    
+}
+
+gozanticruiser::gozanticruiser(int dim, string name, char code){
+    
+    shipSize = dim;
+    
+    shipName = name;
+    
+    shipCode = code;
+    
+    health = shipSize;
+    
+}
+
+int gozanticruiser::getSize() const{
+    return shipSize;
+}
+
+
+int gozanticruiser::getX() const{
+    return theX;
+}
+
+
+int gozanticruiser::getY() const{
+    return theY;
+}
+
+
+string gozanticruiser::getName() const{
+    return shipName;
+}
+
+
+char gozanticruiser::getCode() const{
+    return shipCode;
+}
+
+
+bool gozanticruiser::ShipOrientation() const{
+    return orientation;
+}
+
+
+bool gozanticruiser::ShipDestroyed() const{
+    return destroyed;
+}
+
+
+void gozanticruiser::setShip(int x, int y, bool o){
+    
+    theX = x;
+    theY = y;
+    
+    orientation = o;
+    
+    return;
+}
+
+
+void gozanticruiser::printShip(){
+    
+    for(int i = 0; i < shipSize; i++){
+        
+        cout << shipCode << " ";
+    }
+    
+    cout << endl;
+    
+    return;
+}
+
+
+void gozanticruiser::setHit(){
+    
+    health--;
+    
+}
+
+bool gozanticruiser::isSunk(){
+    
+    if(health == 0){ //se non ha salute è affondata
+        destroyed = true;
+        return true;
+    }else
+        return false;
+        
 }
