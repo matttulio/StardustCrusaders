4c524329506393e9a5a3a0213d9560feec3b9e21
matttulio <101868697+matttulio@users.noreply.github.com>
2022-07-28 17:31:20 +0200

    Create SuperStarDestroyer.hpp

---
+++ IlCodiceQuelloVero/SuperStarDestroyer.hpp
@@ -0,0 +1,52 @@
+//
+//  SuperStarDestroyer.hpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#ifndef SuperStarDestroyer_hpp
+#define SuperStarDestroyer_hpp
+
+#include "ship.hpp"
+
+class superstardestroyer: public ship{
+    
+public:
+    superstardestroyer();
+    superstardestroyer(int dim, string name, char code);  
+    superstardestroyer(int dim, string name, char code, int x, int y, bool d, bool o);
+    
+    virtual int getSize() const;
+    virtual int getX() const;
+    virtual int getY() const;
+    virtual string getName() const;
+    virtual char getCode() const;
+
+
+    virtual bool ShipOrientation() const;
+    virtual bool ShipDestroyed() const;
+    
+    
+    virtual void setShip(int x, int y, bool o);
+    virtual void printShip();
+    virtual void setHit();
+    virtual bool isSunk();
+
+
+private:
+    int shipSize;
+
+    int theX, theY;
+
+    bool destroyed;
+    bool orientation;
+
+    string shipName;
+    char shipCode;
+    
+    int health;
+    
+};
+
+#endif /* SuperStarDestroyer_hpp */
