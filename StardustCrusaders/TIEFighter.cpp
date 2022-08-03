61ea09a7811ea8d1fd98d64ef81c0057004689fc
matttulio <101868697+matttulio@users.noreply.github.com>
2022-08-03 18:35:17 +0200

    Create TIEFighter.cpp

---
+++ StardustCrusaders/TIEFighter.cpp
@@ -0,0 +1,106 @@
+//
+//  TIEFighter.cpp
+//  Stardust Crusaders
+//
+//  Created by Matteo Gallo on 12/07/22.
+//
+
+#include "TIEFighter.hpp"
+
+TIEfighter::TIEfighter(){
+    
+    shipSize = 1;
+    
+    shipName = "TIEfighter";
+    
+    shipCode = 'D';
+    
+    health = shipSize;
+    
+}
+
+TIEfighter::TIEfighter(int dim, string name, char code){
+    
+    shipSize = 1;
+    
+    shipName = "TIEfighter";
+    
+    shipCode = 'D';
+    
+    health = shipSize;
+    
+}
+
+int TIEfighter::getSize() const{
+    return shipSize;
+}
+
+
+int TIEfighter::getX() const{
+    return theX;
+}
+
+
+int TIEfighter::getY() const{
+    return theY;
+}
+
+
+string TIEfighter::getName() const{
+    return shipName;
+}
+
+
+char TIEfighter::getCode() const{
+    return shipCode;
+}
+
+
+bool TIEfighter::ShipOrientation() const{
+    return orientation;
+}
+
+
+bool TIEfighter::ShipDestroyed() const{
+    return destroyed;
+}
+
+
+void TIEfighter::setShip(int x, int y, bool o){
+    
+    theX = x;
+    theY = y;
+    
+    orientation = o;
+    
+    return;
+}
+
+
+void TIEfighter::printShip(){
+    
+    for(int i = 0; i < shipSize; i++){
+        
+        cout << shipCode << " ";
+    }
+    
+    
+    return;
+}
+
+
+void TIEfighter::setHit(){
+    
+    health--;
+    
+}
+
+bool TIEfighter::isSunk(){
+    
+    if(health == 0){ //se non ha salute è affondata
+        destroyed = true;
+        return true;
+    }else
+        return false;
+        
+}
