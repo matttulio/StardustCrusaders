d06c44c5940ccc601e6d9a3bbde46a5fd31f65e5
Awalilly <102657916+Awalilly@users.noreply.github.com>
2022-08-24 19:11:18 +0200

    Update account.hpp

--- StardustCrusaders/account.hpp
+++ StardustCrusaders/account.hpp
@@ -46,7 +46,7 @@ public:
     bool getLogged() const;
     string getUsername() const;
     
-    void writeStats(bool victory, bool against, int n_hit, int n_miss);
+    void writeStats(bool victory, bool against, int n_hit, int n_miss, bool m);
     void getStats();
     
 private:
